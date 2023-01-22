b45b6cd463400240d4028f054898bb137d4d5273
AleRivasSora <hector.escobar.ale@gmail.com>
2023-01-22 00:05:10 -0400

    Agregando nuevas partes al programa

--- Clientes_plantilla.py
+++ Clientes_plantilla.py
@@ -1,2 +1,16 @@
 import Aplicacion_con_sqlo as app
+from tkinter import ttk
+import sqlite3 as sql
+import numpy as np
+import pandas as pd
+import os
+import tkinter.messagebox
+from tkinter import *
 
+def ventana_clientes():
+    global ventana2
+    ventana2 = Toplevel(app.ventanaa1) #crear una ventana siguiente de la ventana1
+    ventana2.geometry("500x500")
+    ventana2.title("ventana2")
+
+    ventana2.mainloop()
