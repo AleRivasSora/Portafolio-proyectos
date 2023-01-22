3f5211ccac2beac802721b6ddcf16e79e24c752e
AleRivasSora <123262411+AleRivasSora@users.noreply.github.com>
2023-01-22 00:18:41 -0400

    Update Aplicacion_con_sqlo.py

--- Aplicacion_con_sqlo.py
+++ Aplicacion_con_sqlo.py
@@ -6,7 +6,6 @@ import os
 import tkinter.messagebox
 from tkinter import *
 
-
 def eliminar():
     seleccionado = datos_mostrar.focus()
     print(seleccionado)
@@ -218,7 +217,7 @@ def ingresar_datos():
         pre = float(precio1.get())
         marc1 = str(marca1.get())
         marc = marc1.capitalize()
-        if nom or marc != "" or pre ==None:
+        if nom and marc != "" or pre ==None:
             conexion=sql.connect("archivos.db")
             conexion.execute("insert into registro_productos(Nombre,Precio,Marca) values (?,?,?)", (nom,pre,marc))
             conexion.commit()
@@ -233,26 +232,60 @@ def ingresar_datos():
         print("error")
         error1 = tkinter.messagebox.showerror("Error","Valor invalido")
         limpiar()
-        
 
-
-    
-    
 i = 0
+def habilitar_productos():
+    '''HABILITA productos'''
+    entry1.config(state=NORMAL)
+    entry2.config(state=NORMAL)
+    entry3.config(state=NORMAL)
+    guardar.config(state=NORMAL)
+    borrar.config(state=NORMAL)
+    modificar.config(state=NORMAL)
+    '''Deshabilita carrito de compras'''
+    entry1_c.config(state=DISABLED)
+    entry3_c.config(state=DISABLED)
+    entry2_c.config(state=DISABLED)
+    entry4_c.config(state=DISABLED)
+    
+    ingresa_c.config(state=DISABLED)
+    añadir_carrito.config(state=DISABLED)
+    comprar.config(state=DISABLED)
+########################################################################################
+############---Funciones Parte Clientes---##############################################
+def habilitar_carrito():
+    '''Deshabilita el frame de productos y modificar productos'''
+    limpiar()
+    limpiarM()
+    entry1.config(state=DISABLED)
+    entry2.config(state=DISABLED)
+    entry3.config(state=DISABLED)
+    guardar.config(state=DISABLED)
+    borrar.config(state=DISABLED)
+    modificar.config(state=DISABLED)
 
+    cancelar_modificado.config(state=DISABLED)
+    guardar_modificado.config(state=DISABLED)
+    entry4.config(state=DISABLED)
+    entry5.config(state=DISABLED)
+    entry6.config(state=DISABLED)
 
-
+    '''Habilita la parte de compras'''
+    entry1_c.config(state=NORMAL)
+    entry3_c.config(state=NORMAL)
+    entry2_c.config(state=NORMAL)
+    entry4_c.config(state=NORMAL)
+    
+    ingresa_c.config(state=NORMAL)
+    añadir_carrito.config(state=NORMAL)
+    comprar.config(state=NORMAL)
 
 if __name__ == "__main__":
 
-
-
     ventana = Tk()
-ventana.geometry("800x600")
-ventana.title("Negocio")
+    ventana.attributes("-fullscreen",True)
+    ventana.title("Negocio")
 
-
-        
 ############################################
 #Parte 1 del programa#######################
 
@@ -261,7 +294,7 @@ precio1= StringVar()
 marca1= StringVar()
 
 frame = LabelFrame(ventana, text= "Introduzca Producto",pady=20,padx=20,font="Arial 20")
-frame.place(relx=0.01,rely=0.01,relheight=0.3,relwidth=0.4)
+frame.place(relx=0.01,rely=0.01,relheight=0.2,relwidth=0.3)
 frame2 = LabelFrame(ventana,pady=20,height=250,width=800).place(relx=0.01,rely=4)
 
 
@@ -295,7 +328,7 @@ datos_mostrar.heading("#2",text="Marca",anchor=CENTER)
 datos_mostrar.place(x=10,y=200)
 datos_mostrar['selectmode']='browse'
 
-etiqueta_ordenar = Label(frame2, text="Ordenar datos por\norden",font="Arial 12").place(x=630,y=200)
+etiqueta_ordenar = Label(frame2, text="Ordenar productos por\norden",font="Arial 12").place(x=630,y=200)
 ordenar_datos1 = Button(frame2,text="Alfabetico",bg="Sky blue",command= lambda:ordenar_alfabetico())
 ordenar_datos1.place(x=650,y=250)
 
@@ -318,7 +351,7 @@ nombre_modificar = StringVar()
 precio_modificar = StringVar()
 marca_modificar = StringVar()
 frame3 = LabelFrame(ventana, text= "Modificar datos de producto",pady=20,padx=20,font="Arial 18")
-frame3.place(relx=0.43,rely=0.01,relheight=0.3,relwidth=0.4)
+frame3.place(relx=0.35,rely=0.01,relheight=0.2,relwidth=0.25)
 entry4 = Entry(frame3,textvariable=nombre_modificar,state=DISABLED)
 entry5 = Entry(frame3,textvariable=precio_modificar,state=DISABLED)
 entry6 = Entry(frame3,textvariable=marca_modificar,state=DISABLED)
@@ -338,6 +371,64 @@ cancelar_modificado = Button(frame3,bg="Red",text="Cancelar",height=1,width=10,c
 cancelar_modificado.grid(row=7,column=2)
 cancelar_modificado.config(state=DISABLED)
 
+'''Habilitar parte de productos'''
+productos = Button(ventana,fg="White",text="Ingresar Productos",bg="green",height=2,width=15,font="arial",command=lambda:habilitar_productos())
+productos.place(x=880,y=50)
+
 conectarDB()
+
+########################################################################################
+############----Parte Clientes----#####################
+
+frame4 = LabelFrame(ventana, text="Ingrese compra",font="Arial 20",labelanchor="n")
+frame4.place(relx=0.33,rely=0.54,relheight=0.3,relwidth=0.3)
+
+nombre_c = Label(frame4,text="Nombre del cliente").grid(row=0,column=0)
+cedula_c = Label(frame4,text="Cedula").grid(row=2,column=0)
+producto_c = Label(frame4,text="Producto").grid(row=4,column=0)
+cantidad_c = Label(frame4,text="Cantidad").grid(row=6,column=0)
+
+nombre_cliente = StringVar()
+cedula_cliente = StringVar()
+producto_cliente = StringVar()
+cantidad_cliente = IntVar()
+
+entry1_c = Entry(frame4,textvariable=nombre_cliente, state=DISABLED)
+entry2_c = Entry(frame4,textvariable=cedula_cliente,state=DISABLED)
+entry3_c = Entry(frame4,textvariable=producto_cliente,state=DISABLED)
+entry4_c = Entry(frame4,textvariable=cantidad_cliente,state=DISABLED)
+entry1_c.grid(row=1,column=0)
+entry2_c.grid(row=3,column=0)
+entry3_c.grid(row=5,column=0)
+entry4_c.grid(row=7,column=0)
+
+ingresa_c = Button(frame4,text="Ingresar Cliente",bg="Light green",state=DISABLED)
+ingresa_c.place(relx=0.55,rely=0.1,relheight=0.15,relwidth=0.3)
+añadir_carrito = Button(frame4,text="Añadir al\ncarrito",bg="Light blue",state=DISABLED)
+añadir_carrito.place(relx=0.55,rely=0.5,relheight=0.15,relwidth=0.3)
+comprar = Button(frame4,text="Comprar",bg="Green",state=DISABLED)
+comprar.place(relx=0.55,rely=0.7,relheight=0.15,relwidth=0.3)
+
+frame5 = Frame(ventana)
+frame5.place(relx=0.01,rely=0.56,relheight=0.5,relwidth=0.3)
+
+mostrar_carrito = ttk.Treeview(frame5,columns=("col1","col2","coli3"))
+mostrar_carrito.heading("#0",text="ID",anchor="center")
+mostrar_carrito.heading("#1",text="Productos",anchor=CENTER)
+mostrar_carrito.heading("#2",text="Cantidad",anchor=CENTER)
+mostrar_carrito.heading("#3",text="Suma total",anchor=CENTER)
+mostrar_carrito.column("#0",width=20)
+mostrar_carrito.column(0,width=120)
+mostrar_carrito.column(1,width=80)
+mostrar_carrito.column(2,width=80)
+mostrar_carrito.pack_configure(fill= X)
+mostrar_carrito['selectmode']='browse'
+
+activar_carrito = Button(ventana,fg="White",font="arial",text="Carrito de\ncompras",height=2,width=15,bg="green",command=lambda:habilitar_carrito())
+activar_carrito.place(x=880,y=120)
+
+
+
+
 
 ventana.mainloop()
